8e31ad8a2b3343d8f36b82db0a46f13f98fe6cca
franxyzxyz <franxyzxyxz@gmail.com>
2015-10-12 12:17:17 +0800

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pikachu-volleyball
-first project
+# Pikachu Volleyball
+
+Intense volleyball game with two Pikachu
+
+URL: https://wdi-hk-9.github.io/Pikachu-Volleyball
--- js/trajectory.js
+++ js/trajectory.js
@@ -6,7 +6,7 @@ $(function(){
   var dt = 1;
   var newTimer;
 
-  var position = {
+  var position = { // ball
     x_0 : 20,
     y_0 : 100,
     Vx : 110,
@@ -29,34 +29,12 @@ $(function(){
       top:position.y
     });
 
-    if (t>20){
+    if (t>20){ // tbc to event hitting another target
       clearInterval(timer);
       t = 0;
       position.up=false;
     }
 
-    // if (position.x<=0 || position.x >= maxX || position.y == 0 || position.y >= maxY){
-    //   clearInterval(timer);
-    //   t=0;
-    //   setNewTimer(position);
-    // };
-
-  }
-
-  function rules(object){
-    if (object.x<=0 || object.x >= maxX){
-      object.Vx = object.Vx * -1;
-      console.log("X" + object.Vx);
-    };
-    if (object.y == 0 || object.y >= maxY){
-      object.Vy = object.Vy * -1;
-
-    };
-
-    object.x = object.x + object.Vx * dt;
-    object.y = object.y + object.Vy * dt;
-
-    update(object.x,object.y);
   }
 
   function update(x,y){
@@ -70,10 +48,6 @@ $(function(){
     timer = setInterval(trajectory, 10);
   }
 
-  function setNewTimer(object){
-    newTimer = setInterval(rules(object),10);
-  }
-
   $(document).keydown(function(e){
     if (e.which == 38  && position.up == false){
       position.up = true;
@@ -81,9 +55,7 @@ $(function(){
     };
     if (e.which == 13 && timer) { //return key
       position.exc = true;
-      // clearInterval(timer);
       console.log($("#pika").offset().left,$("#pika").offset().top, position.x,position.y);
-
     }
   })
 
